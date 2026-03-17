39053b9173d14d805297197c6b9b32713b60a01b
garyboiskin <garyboriskin@gmail.com>
2026-03-17 16:01:39 -0500

    Commit 2

---
+++ src/main/java/com/example/springboothello/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.springboothello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sendGreetings() {
+        return "Hello, World from Gary!";
+    }
+}
